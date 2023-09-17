41739d7a75cbe4ad9c082d1f24dabb3ed9c27e79
Sammara Nunes <sammara.nunes@ccc.ufcg.edu.br>
2023-09-17 10:12:49 -0300

    New functions to signup screen

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
-import { TextInput, Button, Text, VStack, Box } from "@react-native-material/core";
-import DatePicker from 'react-native-datepicker';
+import { SafeAreaView } from 'react-native';
+import { TextInput, Button, Text, VStack, Box, Divider } from "@react-native-material/core";
+import DateTimePicker  from '@react-native-community/datetimepicker';
 
 export default function SignUp({navigation, route}) {
-    const [date, setDate] = useState(new Date())
-    const [open, setOpen] = useState(false)
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setDate(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
 
     return (
         <Box>
@@ -12,8 +29,29 @@ export default function SignUp({navigation, route}) {
                 <TextInput label="Nome" variant="standard" color="#2EBC6B"/>
                 <TextInput label="Sobrenome" variant="standard" color="#2EBC6B"/>
                 <TextInput label="Email" variant="standard" color="#2EBC6B"/>
-                <DatePicker date={date} onDateChange={setDate} />
-                <TextInput label="Peso" variant="standard" color="#2EBC6B"/>
+                
+                <Text variant="h6" >Dados do Pet</Text>
+                <TextInput label="Nome" variant="standard" color="#2EBC6B"/>
+                <SafeAreaView>
+                    <Button onPress={showDatepicker} title="Data de Nascimento" />
+                    {show && (
+                        <DateTimePicker
+                            testID="dateTimePicker"
+                            value={date}
+                            mode={mode}
+                            is24Hour={true}
+                            onChange={onChange}
+                        />
+                    )}
+                </SafeAreaView>
+                 
+                <TextInput 
+                    label="Peso" 
+                    variant="standard"
+                    color="#2EBC6B"
+                    keyboardType='number-pad'
+                    helperText='Em Kg'
+                />
 
                 <TextInput secureTextEntry={true} label="Senha" variant="standard" color="#2EBC6B"/>
                 <TextInput secureTextEntry={true} label="Confirmar Senha" variant="standard" color="#2EBC6B"/>
